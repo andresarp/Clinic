282772f43cca30e61f688afbb5e950358be1c711
aramirezpa <andresalonsoramirezpacheco>
2022-11-16 20:22:03 -0600

    Se agregan estilos a las pantallas de pacientes

--- Clinica/Models/Patient.cs
+++ Clinica/Models/Patient.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Clinica.Models
 {
     public partial class Patient
     {
+        [Required(ErrorMessage = "El campo cédula es obligatorio")]
         public int IdUser { get; set; }
+        [Required(ErrorMessage = "El campo nombre es obligatorio")]
         public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "El campo número es obligatorio")]
         public string PhoneNumber { get; set; } = null!;
     }
 }
